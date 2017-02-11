b53ce99c44e42f63007c4bf611af185a0298a35e
ReggieLai <Regina@reginas-mbp.wifi.bcit.ca>
2017-02-10 23:33:51 -0800

    WeaponsBase
    Day 1 Weapons scripts

---
+++ FlipFlopGame/Pistol.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Weapon {
+
+	// Use this for initialization
+	void Start () {
+		fireRate = 1;
+		reloadRate = 1;
+		clipSize = 10;
+		ammo = clipSize;
+		speed = 1;
+		damage = 1;
+		
+	}
+
+}
---
+++ FlipFlopGame/Weapon.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour {
+	protected double fireRate;
+	protected double reloadRate;
+	protected int clipSize;
+	protected int ammo;
+	protected double speed;
+	protected int damage;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("fire1")) {
+			Shoot ();
+		}
+		
+		if (Input.GetKeyDown ("r")) {
+			Reload ();
+		}
+	}
+
+	// Shoot
+	void Shoot() {
+		if (ammo == 0) {
+			Reload ();
+		} else {
+			ammo -= 1;
+		}
+		// TODO: add dmg, if it hits a block/misses 
+
+	}
+
+	// Reload
+	void Reload() {
+		ammo = clipSize;
+	}
+
+
+}
+
+
