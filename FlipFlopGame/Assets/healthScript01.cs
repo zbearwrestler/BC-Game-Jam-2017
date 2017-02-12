28bd7576c6badf8af106f9b3e0eb60d39ea435f8
zbearwrestler <z.bearinger@gmail.com>
2017-02-12 09:21:31 -0800

    Revert "Everything I could do"
    This reverts commit 6d72afa1ec1e6a85d22d1eab94a354560c45a648.

---
+++ FlipFlopGame/Assets/healthScript01.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healthScript01 : MonoBehaviour {
+
+    public float health = 100f;
+    public bool isPlayerAlive = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log(health);
+        if (health < 0f)
+        {
+            isPlayerAlive = false;
+            Destroy(gameObject);
+        }
+	}
+}
